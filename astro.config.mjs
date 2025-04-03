ec5da7fc6ed94f8ce47059b612876365731c7d5e
Jaime <jaimecriado02@gmail.com>
2025-04-03 11:39:54 +0200

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jaimecm02.github.io',
+  base: '/HistaviWeb/',
   vite: {
     plugins: [tailwindcss()]
   }
